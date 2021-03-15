8eee9672a9c64f2e4788574196043f641c10df00
Rohit Parakh <rohitparakh4@gmail.com>
2021-03-15 20:32:24 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/svm-react/precache-manifest.4978c0340c974d5d5bc1208ac29a2fa9.js"
+  "/svm-react/precache-manifest.863b6494a79ccacd55779795f3f937cf.js"
 );
 
 self.addEventListener('message', (event) => {
